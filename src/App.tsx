3bf86cc81db400ca86c3da349e7aff557654ee87
Javier Garcia Hernandez (ext) <javier.garcia.external@eviden.com>
2025-02-19 12:43:36 +0000

    Creating new branch to set up Cypress inside the project environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer/Footer"
 import "./index.css"
 
 const App: React.FC = () => {
@@ -7,8 +8,9 @@ const App: React.FC = () => {
             <div className="bg-cyan-100">
                 <h1 className="text-center text-lg font-bold mt-6">Hola Mundo</h1>
             </div>
+            <Footer />
         </>
-    );
+    )
 }
 
 export default App
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+
+export const Footer: React.FC = () => {
+    return (
+        <div data-cy-id="footer" className="py-4 bg-amber-200 text-center">
+            Juice Calculator 2025 &#169;
+        </div>
+    )
+}
