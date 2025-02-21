c8e0e6a25221ae6020a3a603a3e5c266a9d0c55a
Javier Garcia Hernandez (ext) <javier.garcia.external@eviden.com>
2025-02-21 14:01:22 +0000

    Current setup enables testing for both e2e and components while not affecting how the application is launched normally

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    reactRouter(),
+    // Disables react-router plugin when running Cypress tests
+    process.env.CYPRESS_INTERNAL_ENV === undefined && reactRouter(),
     tailwindcss(),
   ],
   
